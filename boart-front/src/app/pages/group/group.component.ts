45661543ddd33f18496e52328529b913720afc25
m0scar <osbaizq@gmail.com>
2017-02-06 21:03:12 +0100

    terminada maquetacion de /group

--- boart-front/src/app/pages/group/group.component.ts
+++ boart-front/src/app/pages/group/group.component.ts
@@ -7,7 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupComponent implements OnInit {
 
-  constructor() { }
+  private publicationsMock;
+
+  constructor() {
+    this.publicationsMock = [
+      {
+        img: "http://i797.photobucket.com/albums/yy260/soyhanechan/01.jpg",
+        text: "Donec a fermentum nisi. Integer dolor est, commodo ut sagittis vitae, egestas at augue. Suspendisse id nulla ac urna vestibulum mattis."
+      },
+      {
+        img: "http://mamiverse.com/es/wp-content/uploads/2016/01/T%C3%A9cnicas-fotogr%C3%A1ficas-El-arte-de-las-fotos-art%C3%ADsticas-Photo3.jpg",
+        text: "Donec a fermentum nisi. Integer dolor est, commodo ut sagittis vitae, egestas at augue. Suspendisse id nulla ac urna vestibulum mattis."
+      },
+      {
+        img: "https://fografiasartisticas.files.wordpress.com/2013/04/fotografias-artisticas-2.jpg",
+        text: "Donec a fermentum nisi. Integer dolor est, commodo ut sagittis vitae, egestas at augue. Suspendisse id nulla ac urna vestibulum mattis."
+      },
+      {
+        img: "http://i797.photobucket.com/albums/yy260/soyhanechan/01.jpg",
+        text: "Donec a fermentum nisi. Integer dolor est, commodo ut sagittis vitae, egestas at augue. Suspendisse id nulla ac urna vestibulum mattis."
+      },
+      {
+        img: "http://mamiverse.com/es/wp-content/uploads/2016/01/T%C3%A9cnicas-fotogr%C3%A1ficas-El-arte-de-las-fotos-art%C3%ADsticas-Photo3.jpg",
+        text: "Donec a fermentum nisi. Integer dolor est, commodo ut sagittis vitae, egestas at augue. Suspendisse id nulla ac urna vestibulum mattis."
+      },
+      {
+        img: "https://fografiasartisticas.files.wordpress.com/2013/04/fotografias-artisticas-2.jpg",
+        text: "Donec a fermentum nisi. Integer dolor est, commodo ut sagittis vitae, egestas at augue. Suspendisse id nulla ac urna vestibulum mattis."
+      },      {
+        img: "http://i797.photobucket.com/albums/yy260/soyhanechan/01.jpg",
+        text: "Donec a fermentum nisi. Integer dolor est, commodo ut sagittis vitae, egestas at augue. Suspendisse id nulla ac urna vestibulum mattis."
+      }
+    ]
+  }
 
   ngOnInit() {
   }
