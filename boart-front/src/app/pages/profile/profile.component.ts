ff1bb1aa022708a65e25556d441a86eb148f49ee
Pablo Rivero <p.riverop@alumnos.urjc.es>
2017-02-06 02:14:09 +0100

    Hechos los perfiles (faltan detalles)

--- boart-front/src/app/pages/profile/profile.component.ts
+++ boart-front/src/app/pages/profile/profile.component.ts
@@ -7,7 +7,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  private userInfoMocks;
+  private userPublicationMocks;
+  private userGalleryMocks;
+  private userRRSSMocks;
+	
+  constructor() {
+
+  	this.userInfoMocks = [
+  		{
+  			avatar: "http://www.dagmedya.net/wp-content/uploads/2013/07/Mariano-Rajoy.jpg",
+  			nickname: "@perrorojo",
+  			name: "Alfonso",
+  			surname: "Felipe Muñoz",
+  			aboutme: "Aficionado a la fotografía, me gusta fotografiar naturaleza, en especial aves."
+  		}
+  	],
+
+  	this.userPublicationMocks = [
+  		{
+  			name: "Anochecer con V",
+  			description: "Fotografía de un anochecer en Vallekas City.",
+  			url: "#",
+  			redes : [
+  						{
+							  url: "https://image.flaticon.com/icons/png/128/59/59439.png"
+							},
+							{
+								url: "https://image.flaticon.com/icons/png/128/33/33671.png"
+							},
+							{
+								url: "https://image.flaticon.com/icons/png/128/34/34227.png"
+							}
+				]
+			}
+		],
+  			
+		this.userGalleryMocks = [
+			{
+			  url: "http://cssdeck.com/uploads/media/items/6/6f3nXse.png"
+			},
+			{
+			  url: "http://cssdeck.com/uploads/media/items/1/1swi3Qy.png"
+			},
+			{
+			  url: "http://cssdeck.com/uploads/media/items/2/2v3VhAp.png"
+			},
+			{
+			  url: "http://cssdeck.com/uploads/media/items/6/6f3nXse.png"
+			},
+			{
+			  url: "http://cssdeck.com/uploads/media/items/1/1swi3Qy.png"
+			}
+		]
+
+	}
 
   ngOnInit() {
   }
