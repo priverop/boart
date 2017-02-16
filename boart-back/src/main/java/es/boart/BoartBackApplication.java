2a603e177eb4662e37902aa883bc80bc3044264c
Pablo Rivero <p.riverop@alumnos.urjc.es>
2017-02-17 00:57:17 +0100

    Proyecto nuevo (sin fallos ni asterisco, añadido mustache y mysql). Hecha la portada a falta de CSS y Mustache.

---
+++ boart-back/src/main/java/es/boart/BoartBackApplication.java
@@ -0,0 +1,12 @@
+package es.boart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoartBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoartBackApplication.class, args);
+	}
+}
