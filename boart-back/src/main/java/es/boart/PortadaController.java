3ec167d91e35a7b228c77a098b210c7c7f8587f4
Pablo Rivero <p.riverop@alumnos.urjc.es>
2017-02-18 22:43:10 +0100

    Arreglado el ejemplo de la portada (foto y texto loren ipsum), CSS completo de portada y componentes.

--- boart-back/src/main/java/es/boart/PortadaController.java
+++ boart-back/src/main/java/es/boart/PortadaController.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PortadaController {
+	
 	@RequestMapping("/")
 	public String greeting(Model modelo) {
 
-		modelo.addAttribute("publication.img", "img");
-		modelo.addAttribute("publication.text", "HOLA GABRI JAJ");
+		modelo.addAttribute("publication.img", "http://i797.photobucket.com/albums/yy260/soyhanechan/01.jpg");
+		modelo.addAttribute("publication.text", "Donec a fermentum nisi. Integer dolor est, commodo ut sagittis vitae, egestas at augue. Suspendisse id nulla ac urna vestibulum mattis.");
 		
 		return "portada_template";
 	}
