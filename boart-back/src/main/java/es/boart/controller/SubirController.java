5cbfda5b591e47c5f9f7a009dd28d68b7fd8a2d6
m0scar <osbaizq@gmail.com>
2017-02-23 19:58:37 +0100

    upload básico conseguido

--- boart-back/src/main/java/es/boart/controller/SubirController.java
+++ boart-back/src/main/java/es/boart/controller/SubirController.java
@@ -1,16 +1,27 @@
 package es.boart.controller;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Timestamp;
 import java.util.Date;
 
-import javax.annotation.PostConstruct;
+import org.springframework.core.io.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -34,16 +45,31 @@ public class SubirController {
 	}
 
 	@PostMapping("/upload")
-	public String upload(@RequestParam("file") MultipartFile file, @RequestParam("titulo") String titulo,@RequestParam("descripcion") String descripcion, @RequestParam("etiquetas") String etiquetas) {
+	public String upload(@RequestParam("file") MultipartFile file, @RequestParam("titulo") String titulo,@RequestParam("descripcion") String descripcion, @RequestParam("etiquetas") String etiquetas) throws IOException {
 		// modelo.addAttribute("", repository.findAll());
-		Publicacion publicacion = new Publicacion("m0scar", titulo, descripcion, file.getOriginalFilename(), 0, new Timestamp(new Date().getTime()), 0);
+		Publicacion publicacion = new Publicacion("m0scar", titulo, descripcion,"/files/" + file.getOriginalFilename(), 0, new Timestamp(new Date().getTime()), 0);
 		publicacionRepository.save(publicacion);
 		System.out.println(publicacion.getId()+ " " +publicacion.getAutor() + " " + publicacion.getTitulo() + " " 
 				+ publicacion.getDescripcion() + " " + publicacion.getMedia()  + etiquetas);
 		Tag tag = new Tag(etiquetas);
+	    Path rootLocation = Paths.get("src/main/resources/static/img/");
+		Files.copy(file.getInputStream(), rootLocation.resolve(file.getOriginalFilename()));
 		tag.getPublicaciones().add(publicacion);
 		tagRepository.save(tag);
 		return "publicacion_template";
 	}
+	
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) throws MalformedURLException {
+    	System.out.println(filename);
+    	Path location = Paths.get("src/main/resources/static/img/" + filename);
+        Resource file = new UrlResource(location.toUri());
+                
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+(file).getFilename()+"\"")
+                .body(file);
+    }
 
 }
