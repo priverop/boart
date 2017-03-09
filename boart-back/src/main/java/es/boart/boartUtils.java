8da029430e49a5b4300bfce2689bd85f383855b9
m0scar <osbaizq@gmail.com>
2017-03-09 23:14:48 +0100

    Ya se ve el multimedia en la publicacion

--- boart-back/src/main/java/es/boart/boartUtils.java
+++ boart-back/src/main/java/es/boart/boartUtils.java
@@ -1,11 +1,11 @@
 package es.boart;
 
 public class boartUtils {
-	static String imgEmbedLeft = "<img src=\"/files/";
+	static String imgEmbedLeft = "<img  class=\"img-responsive\" src=\"/files/";
 	static String imgEmbedRight = "\">";
-	static String audioEmbedLeft = "<iframe width=\"100%\" height=\"166\" scrolling=\"no\" frameborder=\"no\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/";
+	static String audioEmbedLeft = "<iframe class=\"iframeAudio\" scrolling=\"no\" frameborder=\"no\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/";
 	static String audioEmbedRight = "&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false\"></iframe>";
-	static String videoEmbedLeft = "<iframe width=\"100%\" height=\"315px\" src=\"https://www.youtube.com/embed/";
+	static String videoEmbedLeft = "<iframe class=\"iframeVideo\" src=\"https://www.youtube.com/embed/";
 	static String videoEmbedRight = "\" frameborder=\"0\"></iframe>";
 	
 	public static String getImgEmbedLeft() {
@@ -26,8 +26,5 @@ public class boartUtils {
 	public static String getVideoEmbedRight() {
 		return videoEmbedRight;
 	}
-	
-	
-
 
 }
