57355dfb133dc729dd935ea75036cee5c8ac6d65
Gabriel <falconeta8@gmail.com>
2017-03-12 12:29:59 +0100

    Edit profile.

--- boart-back/src/main/java/es/boart/repository/UserRepository.java
+++ boart-back/src/main/java/es/boart/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import es.boart.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	User findById(Long id);
 	User findByUsername(String username);
 	User findByUsernameAndPassword(String username, String password);
 
