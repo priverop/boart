56332c6e14812e2ddbe83acf27788a92cd9ceead
Gabriel <falconeta8@gmail.com>
2017-04-02 12:54:18 +0200

    LikeRestController.

---
+++ boart-back/src/main/java/es/boart/rest/LikeRestController.java
@@ -0,0 +1,105 @@
+package es.boart.rest;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.boart.UserComponent;
+import es.boart.model.Publication;
+import es.boart.model.PublicationLike;
+import es.boart.model.User;
+import es.boart.services.LikeService;
+import es.boart.services.PublicationService;
+import es.boart.services.UserService;
+
+@RestController
+@RequestMapping("/api/like")
+public class LikeRestController {
+	
+	@Autowired
+	private UserComponent userSession;
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private LikeService likeService;
+	
+	@Autowired
+	private PublicationService publicationService;
+	
+	@PostMapping("/")
+	public ResponseEntity<PublicationLike> addLike(@RequestParam String publicationId){		
+				
+		if (userSession.getUser() != null) {
+			
+			User user = userService.findOne(userSession.getUser().getId());
+						
+			if (user != null) {
+				
+				Publication publication = publicationService.findOne(Long.parseLong(publicationId));
+				PublicationLike like = likeService.findByPublicationIdAndUserId(publication.getId(), user.getId());
+				
+				if(like == null) {
+					PublicationLike newLike = new PublicationLike(user, publication);
+					publication.addLike(newLike);
+					likeService.save(newLike);
+					publicationService.save(publication);
+					return new ResponseEntity<>(HttpStatus.OK);
+				} else {
+					return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+				} 
+				
+			} else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			} 	
+			
+		} else {
+			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+		}
+			
+	}
+	
+	@DeleteMapping("/{publicationId}")
+	public ResponseEntity<PublicationLike> deleteLike(@PathVariable String publicationId){		
+				
+		if (userSession.getUser() != null) {
+			
+			User user = userService.findOne(userSession.getUser().getId());
+						
+			if (user != null) {
+				
+				Publication publication = publicationService.findOne(Long.parseLong(publicationId));
+				PublicationLike like = likeService.findByPublicationIdAndUserId(publication.getId(), user.getId());
+				
+				if(like == null) {
+					return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+				} else {
+					publication.removeLike(like);
+					likeService.delete(like);
+					publicationService.save(publication);
+					return new ResponseEntity<>(HttpStatus.OK);
+				} 
+				
+			} else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			} 	
+			
+		} else {
+			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+		}
+			
+	}
+
+}
