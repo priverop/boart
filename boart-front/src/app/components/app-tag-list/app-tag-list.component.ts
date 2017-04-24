b9b7f706428829c200e38550d5a04b15be6cbd45
m0scar <osbaizq@gmail.com>
2017-04-24 23:49:35 +0200

    Añadido tags-list component

--- boart-front/src/app/components/app-tag-list/app-tag-list.component.ts
+++ boart-front/src/app/components/app-tag-list/app-tag-list.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-app-tag-list',
+  selector: 'app-tag-list',
   templateUrl: './app-tag-list.component.html',
   styleUrls: ['./app-tag-list.component.scss']
 })
 export class AppTagListComponent implements OnInit {
-
+  @Input() tags;
   constructor() { }
-
   ngOnInit() {
   }
 
