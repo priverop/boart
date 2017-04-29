b27a267a6853f87aa19129a6c520b9ede67448e9
Pablo Rivero <p.riverop@alumnos.urjc.es>
2017-04-29 19:44:33 +0200

    Create group [ERROR: MultipartFile]

--- boart-front/src/app/pages/create-group/create-group.component.ts
+++ boart-front/src/app/pages/create-group/create-group.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import { CoverComponent } from '../../pages/cover/cover.component';
+import { AjaxService } from '../../services/ajax.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-group',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateGroupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ajaxService: AjaxService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  createGroup(event: any, title: string, description: string, image: File){
+    event.preventDefault();
+
+    const endpoint = 'group/create';
+    const options = {
+      title: title,
+      description: description,
+      file: image
+    };
+
+    this.ajaxService.postRequest(endpoint, options).subscribe(
+      response => this.router.navigate(['/'])
+      // Return to the new group
+    );
   }
 
 }
--- boart-front/src/app/pages/login/login.component.ts
+++ boart-front/src/app/pages/login/login.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private loginService: LoginService, private router: Router,) { }
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit() {
   }
